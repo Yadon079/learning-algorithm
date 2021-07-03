28855194033d617396afe49f0bca36430cfd9bf4
hjchoi0207 <chjin95@gmail.com>
2021-07-03 16:58:36 +0900

    feat(hyunjin): [BOJ, 11650] 좌표정렬하기 https://www.acmicpc.net/problem/11650

--- Hyunjin-Choi/boj/boj_11650.cpp
+++ Hyunjin-Choi/boj/boj_11650.cpp
@@ -1,48 +1,25 @@
 #include <iostream>
 #include <algorithm>
-#include<vector>
+#include <vector>
 
 using namespace std;
-
-
-class xypair{
-  public:
-  int x, y;
-
-  xypair(int _x, int _y){
-    x = _x;
-    y = _y;
-  }
-};
-
-bool comapre(xypair &a, xypair &b){
-  if(a.x < b.x){
-    return true;
-  }
-  else if(a.x == b.x){
-    if(a.y < b.y) return true;
-    else return false;
-  }
-  else{
-    return false;
-  }
-}
-
 
 int main()
 {
   ios::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+    vector<pair<int, int>> v;
+    // v.push_back(pair<int, int>(1, -1));
+    int n, x, y;
+    cin >> n;
 
-  int n, x, y;
-  cin >> n;
-  vector<xypair> v;
-  for(int i = 0; i < n; i++){
-    cin >> x >> y;
-    v.push_back(xypair(x,y));
-  }
-  sort(v.begin(), v.end(), comapre);
+    for(int i = 0; i < n; i++){
+      cin >> x >> y;
+      v.push_back(pair<int, int>(x, y));
+    }
 
-  for(int i = 0; i < n; i++){
-    cout << v[i].x << " " << v[i].y << '\n';
-  }
+    sort(v.begin(), v.end());
+    
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i].first << ' ' << v[i].second << '\n';
+    }
 }
