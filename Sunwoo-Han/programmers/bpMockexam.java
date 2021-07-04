0d26bb782aae79307d5688d404a4d18ce6b8d87c
GraceNam <zeidgame@gmail.com>
2021-07-04 19:44:48 +0900

    feat(sunwoo) : [Programmers, 42840] 모의고사
    https://programmers.co.kr/learn/courses/30/lessons/42840

---
+++ Sunwoo-Han/programmers/bpMockexam.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class bpMockexam {
+
+  public int[] solution(int[] answers) {
+    int[] answer = {};
+
+    int[] p1 = { 1, 2, 3, 4, 5 };
+    int[] p2 = { 2, 1, 2, 3, 2, 4, 2, 5 };
+    int[] p3 = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
+
+    int ans1 = 0, ans2 = 0, ans3 = 0;
+
+    for(int i = 0; i < answers.length; i++) {
+      if(p1[i % p1.length] == answers[i]) {
+        ans1++;
+      }
+
+      if(p2[i % p2.length] == answers[i]) {
+        ans2++;
+      }
+
+      if(p3[i % p3.length] == answers[i]) {
+        ans3++;
+      }
+    }
+
+    int max = Math.max(Math.max(ans1, ans2), ans3);
+
+    ArrayList<Integer> list = new ArrayList<>();
+
+    if(max == ans1) {
+      list.add(1);
+    }
+
+    if(max == ans2) {
+      list.add(2);
+    }
+
+    if(max == ans3) {
+      list.add(3);
+    }
+
+    answer = new int[list.size()];
+
+    for(int i = 0; i < answer.length; i++) {
+      answer[i] = list.get(i);
+    }
+
+    return answer;
+  }
+}
