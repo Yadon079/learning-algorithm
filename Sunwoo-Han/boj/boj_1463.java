2bd7bb7512b33b4eb7ef9fa454dd8e826bf2b081
yadon079 <zeidgame@gmail.com>
2021-07-06 08:41:24 +0900

    docs : 코드 수정
    <description>
    + boj_1463 코드 수정
      + 예외 handler 추가
    <issue-number>

--- Sunwoo-Han/boj/boj_1463.java
+++ Sunwoo-Han/boj/boj_1463.java
@@ -3,7 +3,7 @@ import java.io.*;
 
 public class boj_1463 {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     int n = Integer.parseInt(br.readLine());
