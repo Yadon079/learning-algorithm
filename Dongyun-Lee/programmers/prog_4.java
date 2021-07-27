8625967f6c19365d3ed69f7295047cb1d557feee
dongyyun <nice4566@gmail.com>
2021-07-28 00:52:48 +0900

    feat(dongyun): [Programmers] 키패드 누르기

--- Dongyun-Lee/programmers/prog_4.java
+++ Dongyun-Lee/programmers/prog_4.java
@@ -7,34 +7,38 @@ public class prog_4 {
     int l_index=0, r_index=0;
 
     for (int i = 0; i < numbers.length; i++) {
-      
-      if (numbers[i] == 0) numbers[i] = 11;
 
+      if (numbers[i] == 0) numbers[i] = 11;
+     
       if (numbers[i] % 3 == 1) {
         answer.append("L");
         left = numbers[i];
-      } else if (numbers[i] % 3 == 0) {
+      } else if (numbers[i] % 3 == 0 && numbers[i] != 0) {
         answer.append("R");
         right = numbers[i];
       } else {
-              
+
+                     
         l_index = (Math.abs(numbers[i] - left))/3 + (Math.abs(numbers[i] - left))%3;
         r_index = (Math.abs(numbers[i] - right))/3 + (Math.abs(numbers[i] - right))%3;
 
-        if (l_index < r_index) {
-          answer.append("L");
-          left = numbers[i];
-        } else if (r_index < l_index) {
-          answer.append("R");
-          right = numbers[i];
-        } else {
-          if(hand == "left") {
+        if (l_index == r_index) {
+
+          if(hand.equals("left")) {
             answer.append("L");
             left = numbers[i];
           } else {
             answer.append("R");
             right = numbers[i];
           }
+          
+        } else if (r_index < l_index) {
+          answer.append("R");
+          right = numbers[i];
+        } else {
+
+          answer.append("L");
+          left = numbers[i];
         }
       }
     }
