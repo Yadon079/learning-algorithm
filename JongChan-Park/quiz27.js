c84fa2e289193319255d472384aefb56de252e0f
JChan91 <fourbin1221@gmail.com>
2021-07-29 08:58:29 +0900

    feat(JongChan) [Programmers, Level 1] 핸드폰 번호 가리기

---
+++ JongChan-Park/quiz27.js
@@ -0,0 +1,6 @@
+// 핸드폰 번호 가리기
+// https://programmers.co.kr/learn/courses/30/lessons/12948
+
+function solution(phone_number) {
+  return phone_number.slice(-4).padStart(phone_number.length, "*");
+}
