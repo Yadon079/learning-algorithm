b4ae8efcbb7cd4ae4abaa8503587ef236b07cbaf
dongyyun <nice4566@gmail.com>
2021-08-01 21:27:48 +0900

    feat(dongyun): [Programmers] 숫자 문자열과 영단어

---
+++ Dongyun-Lee/programmers/prog_5.java
@@ -0,0 +1,18 @@
+public class prog_5 {
+  
+  public int solution(String s) {
+    int answer = 0;
+    
+    String num[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    String str[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
+    for(int i=0;i<10;i++){
+        
+        s = s.replace(str[i], num[i]);
+    }
+    
+    answer = Integer.parseInt(s);
+    
+    return answer;
+}
+}
