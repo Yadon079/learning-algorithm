a99f499149da38640da3ca05285d35230e7858ad
hjchoi0207 <chjin95@gmail.com>
2021-08-04 23:12:59 +0900

    feat(hyunjin) : [Programmers, 76501] 음양 더하기 https://programmers.co.kr/learn/courses/30/lessons/76501

---
+++ Hyunjin-Choi/programmers/sumNegativePositiveNumber.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    for(int i = 0; i < absolutes.size(); i++)
+        answer += (signs[i] == true) ? absolutes[i] : -absolutes[i];
+    return answer;
+}
