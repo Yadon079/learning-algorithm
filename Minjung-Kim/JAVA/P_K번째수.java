3e5d42dabb29021e9b7632370946d8637e7fb78b
santos019 <santos019@naver.com>
2021-08-05 10:04:59 +0900

    feat(<Minjung-Kim>): [프로그래머스, 정렬] K번째수

--- Minjung-Kim/JAVA/P_K번째수.java
+++ Minjung-Kim/JAVA/P_K번째수.java
@@ -1,24 +1,30 @@
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
+        int[] cln;
         int tmp=0;
         for(int i=0;i<commands.length;i++)
         {
-
-            for(int k=commands[i][0];k<commands[i][1];k++)
-            {
-                for(int o=k+1; o<commands[i][1]; o++)
-                {
-                    if(array[k]>=array[o])
+            cln= array.clone();
+            for(int k=(commands[i][0]-1);k<(commands[i][1]);k++)
+            {  System.out.println("k="+k);
+                for(int o=k+1; o<(commands[i][1]); o++)
+                {   System.out.println("o="+o);
+                    if(cln[k]>=cln[o])
                     {
-                        tmp=array[o];
-                        array[o]=array[k];
-                        array[k]=tmp;
+                        tmp=cln[o];
+                        cln[o]=cln[k];
+                        cln[k]=tmp;
                     }
                 }
             }
-            System.out.println(commands[i][0]-2+commands[i][2]);
-            answer[i]=array[commands[i][0]-2+commands[i][2]];
+            for(int j=0;j<cln.length;j++)
+            {
+                System.out.print(cln[j]);
+            }
+            System.out.println();
+            //System.out.println(cln[commands[i][0]-2+commands[i][2]]);
+            answer[i]=cln[commands[i][0]-2+commands[i][2]];
         }
         return answer;
     }
