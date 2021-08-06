309a4aa6049f5ad77488cc6ddf8221be6a6e2f80
dongyyun <nice4566@gmail.com>
2021-08-06 23:22:44 +0900

    feat(dongyun): [BOJ,10773] 제로

---
+++ Dongyun-Lee/boj/boj_10773.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class boj_10773 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int K = sc.nextInt();
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i=0;i<K;i++){
+
+            int num = sc.nextInt();
+
+            if(num==0){
+                stack.pop();
+            }else{
+                stack.push(num);
+            }
+        }
+
+        int sum=0;
+        for(Integer i:stack){
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+    }
+}
