29cca69b2bbcfb419878a195b2bf54a2e9a89beb
hjchoi0207 <chjin95@gmail.com>
2021-08-09 22:06:14 +0900

    feat(hyunjin) : [Programmers, 83201] 위클리 챌린지 2주차 https://programmers.co.kr/learn/courses/30/lessons/83201

---
+++ Hyunjin-Choi/programmers/weeklyChallenge2.cpp
@@ -0,0 +1,82 @@
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int getMinNum(vector<vector<int>> v, int index){
+    int minNum = 101;
+    for(int i = 0; i < v.size(); i++){
+        if(v[i][index] < minNum){
+            minNum = v[i][index];
+        }
+    }
+    return minNum;
+}
+
+int getMaxNum(vector<vector<int>> v, int index){
+    int maxNum = -1;
+    for(int i = 0; i < v.size(); i++){
+        if(v[i][index] > maxNum){
+            maxNum = v[i][index];
+            // [index][i];  [i][index];
+        }
+    }
+    return maxNum;
+}
+
+// 자기 자신을 평가한 점수가 min, max 인지, 겹치는지 확인 
+bool findMinOrMax(vector<vector<int>> v, int index){
+    int minNum, maxNum;
+
+    for(int i = 1; i < v.size(); i++){
+        // minNum = min(minNum, v[index][i]);
+        minNum = getMinNum(v, index);
+        // maxNum = max(maxNum, v[index][i]);
+        maxNum = getMaxNum(v, index);
+    }
+
+    if(v[index][index] == minNum) {
+        if(count(v[index].begin(), v[index].end(), minNum) == 1) return true; 
+    }
+    else if(v[index][index] == maxNum){
+        if(count(v[index].begin(), v[index].end(), maxNum) == 1) return true;
+    }
+    else return false;
+}
+
+
+
+
+string solution(vector<vector<int>> scores) {
+    string answer = "";
+    int sum = 0, len = scores.size();
+    float avg;
+    
+    // 자기 자신이 평가한 점수가 min, max 라면 -1 로 
+    for(int i = 0; i < len; i++){
+        if(findMinOrMax(scores, i))  
+            scores[i][i] = -1;
+    }
+    
+
+    for(int i = 0; i < len; i++){
+        int div = len; // 나누기
+        sum = 0;
+        for(int j = 0; j < len; j++){
+            if(scores[j][i] == -1)
+                div = len - 1;
+            else 
+                sum += scores[j][i];
+        }
+        avg = sum/div;
+        if(avg >= 90) answer += "A";
+        else if(avg >= 80) answer += "B";
+        else if(avg >= 70) answer += "C";
+        else if(avg >= 50) answer += "D";
+        else answer += "F";
+    }
+    
+    return answer;
+}
