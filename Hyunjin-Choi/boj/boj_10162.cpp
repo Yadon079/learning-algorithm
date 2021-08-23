90fc10092901f1f5a54b8b592804070df6d89c4f
hjchoi0207 <chjin95@gmail.com>
2021-08-23 20:35:05 +0900

    feat(hyunjin)[BOJ, 10162] 전자레인지

--- Hyunjin-Choi/boj/boj_10162.cpp
+++ Hyunjin-Choi/boj/boj_10162.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main(){
-    int a = 0,b = 0,c = 0;
+    int a ,b ,c;
     int t;
     cin >> t;
 
-    a += t/300;
+    a = t/300;
     t %= 300;
 
-    b += t/60;
+    b = t/60;
     t %= 60;
 
-    c += t/10;
+    c = t/10;
     t %= 10;
 
     if(t != 0){
