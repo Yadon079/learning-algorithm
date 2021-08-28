92757a260bef944114e71f16f4720f657f067cee
hjchoi0207 <chjin95@gmail.com>
2021-08-28 21:49:53 +0900

    feat(hyunjin)[BOJ, 1439] 뒤집기

--- Hyunjin-Choi/boj/boj_1439.cpp
+++ Hyunjin-Choi/boj/boj_1439.cpp
@@ -7,26 +7,20 @@ int main(){
     string s;
     cin >> s;
     int cnt_0 = 0, cnt_1= 0, res;
-    char c;
 
-    
-    // if(s[0] == '0') cnt_0++;
-    // else cnt_1++;
-
-    for(int i = 0; i < s.length() - 1; i++){
-        if(i+1 == s.length() - 1){
-            if(s[i] == '0') cnt_0++;
-            else if(s[i] == '1') cnt_1++;
+    for(int i = 0; i < s.length(); i++){
+        if(i == 0) continue;
+        if(s[i] != s[i-1]){
+            if(s[i-1] == '0') cnt_0++;
+            else cnt_1++;
         }
-        if(s[i] == s[i+1]) {
-            continue;
-        }
-        else{
+        if(i == s.length() - 1){
             if(s[i] == '0') cnt_0++;
-            else if(s[i] == '1') cnt_1++;
+            else cnt_1++;
         }
     }
-    cout << cnt_0 << endl << cnt_1 << endl;
+
+    // cout << cnt_0 << endl << cnt_1 << endl;
     res = (cnt_0 > cnt_1) ? cnt_1 : cnt_0;
     cout << res;
 }
