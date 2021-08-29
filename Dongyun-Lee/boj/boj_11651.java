161907a36b57e2519c7412c7a4e1470e627440f1
dongyyun <nice4566@gmail.com>
2021-08-29 22:28:33 +0900

    feat(dongyun): [BOJ, 11651] 좌표 정렬하기2

---
+++ Dongyun-Lee/boj/boj_11651.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class boj_11651 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+
+            arr[i][1] = in.nextInt();
+            arr[i][0] = in.nextInt();
+        }
+
+        Arrays.sort(arr, (x, y) -> {
+            if (x[0] == y[0]) {
+                return x[1] - y[1];
+            } else {
+                return x[0] - y[0];
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+
+            System.out.println(arr[i][1] + " " + arr[i][0]);
+        }
+    }
+}
